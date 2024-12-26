9613e669c15d6306bb2af34947a0191d3ac41c02
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-12-26 11:13:30 +0500

    Smooth Scroll, Framer Motion and some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,64 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import FeatureSection from './components/FeatureSection'
-import Workflow from './components/Workflow'
-import Pricing from './components/Pricing'
-import Testimonials from './components/Testimonials'
-import Footer from './components/Footer'
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+import { motion, useSpring, useScroll } from "framer-motion";
 
-function App() {
+export default function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
     <>
-    <Navbar />
-    <div className="max-w-7xl mx-auto pt-20 px-6">
-      <HeroSection />
-      <FeatureSection />
-      <Workflow />
-      <Pricing />
-      <Testimonials />
-      <Footer />
-    </div>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX,
+          position: "fixed",
+          zIndex: 100,
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 4,
+          originX: 0,
+          background: "linear-gradient(to right, #f47016, #9c3512)",
+        }}
+      />
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+// function App() {
+//   return (
+//     <>
+//     <Navbar />
+//     <div className="max-w-7xl mx-auto pt-20 px-6">
+//       <HeroSection />
+//       <FeatureSection />
+//       <Workflow />
+//       <Pricing />
+//       <Testimonials />
+//       <Footer />
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,32 +1,40 @@
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-        VirtualR build tools
-        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
-          {" "}
-          for developers
-        </span>
-      </h1>
-      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-        Empower your creativity and bring your VR app ideas to life with our
-        intutive developments tools. Get started today and turn your imagination
-        into immersive reality!
-      </p>
-      <div className="flex justify-center my-10">
-        <a
-          href="#"
-          className="bg-gradient-to-r from-orange-500 to-red-800 py-3 px-4 mx-3 rounded-md"
-        >
-          Start for free
-        </a>
-        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
-          Documentation
-        </a>
-      </div>
+      <motion.span
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 1.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+      >
+        <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+          VirtualR build tools
+          <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+            {" "}
+            for developers
+          </span>
+        </h1>
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+          Empower your creativity and bring your VR app ideas to life with our
+          intutive developments tools. Get started today and turn your
+          imagination into immersive reality!
+        </p>
+        <div className="flex justify-center my-10">
+          <a
+            href="#"
+            className="bg-gradient-to-r from-orange-500 to-red-800 py-3 px-4 mx-3 rounded-md"
+          >
+            Start for free
+          </a>
+          <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+            Documentation
+          </a>
+        </div>
+      </motion.span>
       <div className="flex mt-10 justify-center">
         <video
           autoPlay
